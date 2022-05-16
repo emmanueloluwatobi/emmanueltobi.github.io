7435477c1bb20b7f2e4ec0989636351324a88c82
emmanueloluwatobi <103076421+emmanueloluwatobi@users.noreply.github.com>
2022-05-16 22:32:51 +0100

    Portfolio

---
+++ Porfolio Project/main.js
@@ -0,0 +1,74 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+
+//Services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const LearnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+    serviceModals[modalClick].classList.add("active");
+}
+
+LearnMoreBtns.forEach((LearnMoreBtn, i) => {
+    LearnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+
+
+//Portfolio section - Modal
+
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+
+
+
+//Our clients - Swiper
+
+//Website dark/light theme
+
+//Scroll to top button
+
+//Navigation menu items active on page scroll
+
+//Responsive navigation menu toggle
+
+//Scroll reveal animations
+//Common reveal options to create reveal animations
+
+//Target elements, and specify options to create reveal animations
